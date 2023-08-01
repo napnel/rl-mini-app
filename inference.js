0ca5b434c4bd5d2c0d1afd8e42a7af683fd8ab47
napnel <yoshi3njm@gmail.com>
2023-08-01 16:02:29 +0900

    Try to introduce rllib with onnxruntime

---
+++ inference.js
@@ -0,0 +1,17 @@
+// import * as ort from "onnxruntime-web";
+
+
+console.log("Hello World! from test.js")
+// console.log(ort.version());
+
+// // Load the model and create InferenceSession
+// const modelPath = "rl/policy_model/model.onnx";
+// const session = await ort.InferenceSession.create(modelPath);
+
+// // Load and preprocess the input obs to inputTensor
+// const obs = [0.1, 0.2, 0.3, 0.4];
+// const inputTensor = new ort.Tensor("float32", obs, [1, 4]);
+
+// // Run inference
+// const outputs = await session.run({ input: inputTensor });
+// console.log(outputs);
---
+++ rl/envs.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+import numpy as np
+
+
+class Board(gym.Env):
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
+        pass
